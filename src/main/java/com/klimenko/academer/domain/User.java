b08ee52366837ffe5bfd48f374573d0792fede4c
Maxym Klymenko <maxym.klimenko@gmail.com>
2015-11-29 22:38:18 +0300

    changing User class

--- src/main/java/com/klimenko/academer/domain/User.java
+++ src/main/java/com/klimenko/academer/domain/User.java
@@ -1,5 +1,28 @@
 package com.klimenko.academer.domain;
 
-public class User {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "USER")
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = -5527566248002296042L;
+	
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+	  
+	@Column(name = "LAST_NAME")
+	private String lastName;
 }
