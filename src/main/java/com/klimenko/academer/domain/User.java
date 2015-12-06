07424e30fb266eaee2015b83ac88ccb7e216ad23
Maxym Klymenko <maxym.klimenko@gmail.com>
2015-12-06 21:06:05 +0300

    Added getters ans setters to User

--- src/main/java/com/klimenko/academer/domain/User.java
+++ src/main/java/com/klimenko/academer/domain/User.java
@@ -25,4 +25,30 @@ public class User implements Serializable{
 	  
 	@Column(name = "LAST_NAME")
 	private String lastName;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 }
---
+++ src/main/java/com/klimenko/academer/service/UserService.java
@@ -0,0 +1,5 @@
+package com.klimenko.academer.service;
+
+public class UserService {
+	
+}
